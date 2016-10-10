f798e203594416c7432d1a24b8c91578b6e2346e
A. Unique TensorFlower <gardener@tensorflow.org>
2016-10-10 10:26:22 -0800

    Merge changes from github. Change: 135698415

--- tensorflow/core/platform/windows/windows_file_system.h
+++ tensorflow/core/platform/windows/windows_file_system.h
@@ -1,4 +1,4 @@
-/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2015 Google Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,27 +13,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_CORE_PLATFORM_RETRYING_FILE_SYSTEM_H_
-#define TENSORFLOW_CORE_PLATFORM_RETRYING_FILE_SYSTEM_H_
+#ifndef TENSORFLOW_CORE_PLATFORM_WINDOWS_WINDOWS_FILE_SYSTEM_H_
+#define TENSORFLOW_CORE_PLATFORM_WINDOWS_WINDOWS_FILE_SYSTEM_H_
 
-#include <string>
-#include <vector>
-#include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/platform/file_system.h"
+
+#ifdef PLATFORM_WINDOWS
+#undef DeleteFile
+#endif
 
 namespace tensorflow {
 
-/// A wrapper to add retry logic to another file system.
-class RetryingFileSystem : public FileSystem {
+class WindowsFileSystem : public FileSystem {
  public:
-  RetryingFileSystem(std::unique_ptr<FileSystem> base_file_system,
-                     int64 delay_microseconds = 1000000)
-      : base_file_system_(std::move(base_file_system)),
-        initial_delay_microseconds_(delay_microseconds) {}
+  WindowsFileSystem() {}
+
+  ~WindowsFileSystem() {}
 
   Status NewRandomAccessFile(
-      const string& filename,
-      std::unique_ptr<RandomAccessFile>* result) override;
+      const string& fname, std::unique_ptr<RandomAccessFile>* result) override;
 
   Status NewWritableFile(const string& fname,
                          std::unique_ptr<WritableFile>* result) override;
@@ -42,37 +40,32 @@ class RetryingFileSystem : public FileSystem {
                            std::unique_ptr<WritableFile>* result) override;
 
   Status NewReadOnlyMemoryRegionFromFile(
-      const string& filename,
+      const string& fname,
       std::unique_ptr<ReadOnlyMemoryRegion>* result) override;
 
   bool FileExists(const string& fname) override;
 
   Status GetChildren(const string& dir, std::vector<string>* result) override;
 
-  Status GetMatchingPaths(const string& dir,
-                          std::vector<string>* result) override;
-
   Status Stat(const string& fname, FileStatistics* stat) override;
 
   Status DeleteFile(const string& fname) override;
 
-  Status CreateDir(const string& dirname) override;
+  Status CreateDir(const string& name) override;
 
-  Status DeleteDir(const string& dirname) override;
+  Status DeleteDir(const string& name) override;
 
-  Status GetFileSize(const string& fname, uint64* file_size) override;
+  Status GetFileSize(const string& fname, uint64* size) override;
 
   Status RenameFile(const string& src, const string& target) override;
 
-  Status IsDirectory(const string& dir) override;
-
- private:
-  std::unique_ptr<FileSystem> base_file_system_;
-  const int64 initial_delay_microseconds_;
-
-  TF_DISALLOW_COPY_AND_ASSIGN(RetryingFileSystem);
+  string TranslateName(const string& name) const override {
+    return name;
+  }
 };
+
+Status IOError(const string& context, int err_number);
 
 }  // namespace tensorflow
 
-#endif  // TENSORFLOW_CORE_PLATFORM_RETRYING_FILE_SYSTEM_H_
+#endif  // TENSORFLOW_CORE_PLATFORM_WINDOWS_WINDOWS_FILE_SYSTEM_H_
